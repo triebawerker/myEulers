a2f758bda2a5ad41681ede4d7b8ea17ce69ebf1c
micha trieba <micha.trieba@mayflower.de>
2015-02-23 23:24:29 +0100

    changed test wording

--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -1,8 +1,8 @@
 require 'rspec'
 require_relative '../lib/palindrome'
 
-describe 'A two_digits_palindrome from a 3-digit number' do
-  it 'should count a 3 digit max_palindrome' do
+describe 'Get palindrome from a 3-digit number' do
+  it 'should find the max palindrome from a 3-digits number' do
     expect(three_digits_palindrome).to eq 906609
   end
 end
